290090ad345241715d1941685d31cf79a6bbaefb
Lais Arena <laisarena@gmail.com>
2021-08-29 19:05:30 -0300

    Create philos

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,26 +6,52 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 20:25:04 by lfrasson          #+#    #+#             */
-/*   Updated: 2021/08/28 21:41:42 by lfrasson         ###   ########.fr       */
+/*   Updated: 2021/08/29 19:04:34 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*ptr;
+
+	ptr = s;
+	while (n--)
+		*ptr++ = 0;
+}
+
+int	create_philos(t_param *param)
+{
+	int	size;
+
+	size = param->number_of_philo;
+	param->philos = (t_philo *)malloc(sizeof(t_philo) * (size + 1));
+	if (!param->forks)
+		return (FAIL);
+	ft_bzero(param->philos, sizeof(t_philo));
+	return (SUCCESS);
+}
 
 int	create_forks(t_param *param)
 {
 	int	size;
 
 	size = param->number_of_philo;
-	param->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * size);
+	param->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)
+			* (size + 1));
 	if (!param->forks)
-		return (FAIL);	
+		return (FAIL);
+	ft_bzero(param->forks, sizeof(pthread_mutex_t));
 	return (SUCCESS);
 }
 
 int	initialize_structures(t_param *param)
 {
-	create_forks(param);
+	if (create_forks(param) == FAIL)
+		return (FAIL);
+	if (create_philos(param) == FAIL)
+		return (FAIL);
 	return (SUCCESS);
 }
 
